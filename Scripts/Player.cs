b807d0e871ee7b778e873a139e76f56a55bafe6b
GhostGramm <marshalonah@gmail.com>
2021-08-07 19:10:04 +0100

    Player Movement update

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,11 +3,13 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    private Vector2 velocity = new Vector2(0,0);
+    private Vector2 velocity = new Vector2(0, 0);
     private int moveSpeed = 100;
+    private int jumpHeight = 1000;
+    private int gravity = 30;
     public override void _Ready()
     {
-        
+
     }
 
     public override void _PhysicsProcess(float delta)
@@ -17,22 +19,32 @@ public class Player : KinematicBody2D
 
     public void PlayerMovement()
     {
-        if(Input.IsActionPressed("ui_left"))
+        if (Input.IsActionPressed("ui_left"))
         {
             velocity.x = -moveSpeed;
         }
-        if(Input.IsActionPressed("ui_right"))
+        if (Input.IsActionPressed("ui_right"))
         {
             velocity.x = moveSpeed;
         }
-        MoveAndSlide(velocity);
+        if (IsOnFloor())
+        {
+            if (Input.IsActionJustPressed("jump"))
+            {
+                velocity.y = -jumpHeight;
+            }
+        }
 
-        velocity.x = Mathf.Lerp(velocity.x,0,0.1f);
+        velocity.y += gravity;
+
+        velocity = MoveAndSlide(velocity, Vector2.Up);
+
+        velocity.x = Mathf.Lerp(velocity.x, 0, 0.1f);     // interpolates between 2 points to stop the player gradually;
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
 }
