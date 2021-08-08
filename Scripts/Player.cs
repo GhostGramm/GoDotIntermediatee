7f8a866aba90e61b2e656565b6f7de3eb5b2b06d
GhostGramm <marshalonah@gmail.com>
2021-08-08 21:14:24 +0100

    implemented move and jump animations

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,9 +7,10 @@ public class Player : KinematicBody2D
     private int moveSpeed = 100;
     private int jumpHeight = 1000;
     private int gravity = 30;
+    private AnimatedSprite animatedSprite;
     public override void _Ready()
     {
-
+        animatedSprite = GetNode<AnimatedSprite>("animatedSprite");
     }
 
     public override void _PhysicsProcess(float delta)
@@ -22,17 +23,29 @@ public class Player : KinematicBody2D
         if (Input.IsActionPressed("ui_left"))
         {
             velocity.x = -moveSpeed;
+            animatedSprite.FlipH = true;
+            animatedSprite.Play("walk");
         }
-        if (Input.IsActionPressed("ui_right"))
+        else if (Input.IsActionPressed("ui_right"))
         {
             velocity.x = moveSpeed;
+            animatedSprite.FlipH = false;
+            animatedSprite.Play("walk");
+        }
+        else
+        {
+            animatedSprite.Play("idle");
         }
         if (IsOnFloor())
         {
-            if (Input.IsActionJustPressed("jump"))
+            if (Input.IsActionPressed("jump"))
             {
                 velocity.y = -jumpHeight;
             }
+        }
+        if(!IsOnFloor())
+        {
+            animatedSprite.Play("jump");
         }
 
         velocity.y += gravity;
